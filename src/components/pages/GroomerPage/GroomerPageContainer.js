b5e2dbb8c73f73e127a09074043056771a31ea16
abrobins <abrgroup@gmail.com>
2020-10-14 20:45:49 -0400

    added component file structure for groomer profile pages

---
+++ src/components/pages/GroomerPage/GroomerPageContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import RenderGroomerPage from './RenderGroomerPage';
+
+function GroomerPageContainer({ LoadingComponent }) {
+  return (
+    <>
+      <RenderGroomerPage />
+    </>
+  );
+}
+
+export default GroomerPageContainer;
---
+++ src/components/pages/GroomerPage/RenderGroomerPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+
+function RenderGroomerPage(props) {
+  return (
+    <div>
+      <h1>Welcome to the Groomer Page</h1>
+    </div>
+  );
+}
+export default RenderGroomerPage;
