86a77adb9b9cc74a0c7b50bd1b4790fe9c1710d2
rdowd <rjarrettdowd@yahoo.com>
2020-10-20 21:09:48 -0400

    Added initial values to form inputs to pre-copile inputs from user info.

--- src/components/ProfileFormPO/FormPOContainer.js
+++ src/components/ProfileFormPO/FormPOContainer.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import RenderFormPO from './RenderFormPO';
 
-const FormPOContainer = () => {
-  return <RenderFormPO />;
+const FormPOContainer = props => {
+  const onFinish = values => {
+    console.log(values);
+  };
+  const onFailed = errorInfo => {
+    console.log(errorInfo);
+  };
+
+  return (
+    <RenderFormPO
+      onFinish={onFinish}
+      onFailed={onFailed}
+      userInfo={props.userInfo}
+    />
+  );
 };
 
 export default FormPOContainer;
