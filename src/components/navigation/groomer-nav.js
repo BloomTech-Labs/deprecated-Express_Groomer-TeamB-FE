cd92c2457a36d85635d8a72796ed3d6a387ed9cd
Shadewalker1984 <Shadewalker1984@yahoo.com>
2020-10-21 19:26:37 -0500

    spruced up the code, returned my testing page back to normal, pushing the new navigation component

--- src/components/navigation/groomer-nav.js
+++ src/components/navigation/groomer-nav.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import './groomer-nav.css';
 import 'antd/dist/antd.css';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
-import LoginContainer from '../pages/Login/LoginContainer';
-import LandingContainer from '../pages/Landing/LandingContainer';
-import ExampleList from '../pages/ExampleList/ExampleListContainer';
-import ProfileList from '../pages/ProfileList/ProfileListContainer';
+import { Link } from 'react-router-dom';
 
 function GroomerHoverNav() {
   return (
@@ -15,10 +11,6 @@ function GroomerHoverNav() {
           <p> &gt; </p>
         </div>
         <div className="anchors">
-          {/* <a href="#">Home</a>
-          <a href="#">Login</a>
-          <a href="#">Exmaples</a>
-          <a href="#">profileList</a> */}
           <Link className="anchor" to="/">
             Home
           </Link>
