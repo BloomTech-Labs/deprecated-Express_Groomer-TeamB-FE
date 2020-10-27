5590c547abf26f72346494065fb0d8089bf4d560
mikejeter <mjeter45@yahoo.com>
2020-10-27 16:47:17 -0400

    feature/GroomerProfilePage

--- src/components/common/ProfileArea.js
+++ src/components/common/ProfileArea.js
@@ -5,16 +5,17 @@ function ProfileArea(props) {
   return (
     <div>
       <h1>Profile for {props.username}</h1>
-      <ul>
-        <li>Email address: {props.emailaddress}</li>
-      </ul>
+      <h2>User Info</h2>
+      <h3>Name: {props.name}</h3>
+      <h3>Address: {props.address}</h3>
     </div>
   );
 }
 
 ProfileArea.propTypes = {
   username: PropTypes.string.isRequired,
-  emailaddress: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  address: PropTypes.string.isRequired,
 };
 
 export default ProfileArea;
