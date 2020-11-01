5ec04e68c4d0415bda8b6e0ec3b24c6e12074332
rdowd <rjarrettdowd@yahoo.com>
2020-10-31 23:28:08 -0400

    Added logic to grab user info from okta for test purposes this data should be grabbed from state managment if possible.

--- src/components/pages/CustomerProfile/CustProContaner.js
+++ src/components/pages/CustomerProfile/CustProContaner.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import RenderCustPro from './RenderCustPro';
+import { useOktaAuth } from '@okta/okta-react';
 
 const CustProContainer = () => {
-  return <RenderCustPro />;
+  //grabbing user info from okta for test purposes
+  //all info could/should be grabbed by state if possible
+  const { authService } = useOktaAuth();
+  const [userInfo, setUserInfo] = useState(null);
+  const [memoAuthService] = useMemo(() => [authService], []);
+
+  useEffect(() => {
+    let isSubscribed = true;
+
+    memoAuthService
+      .getUser()
+      .then(info => {
+        // if user is authenticated we can use the authService to snag some user info.
+        // isSubscribed is a boolean toggle that we're using to clean up our useEffect.
+        if (isSubscribed) {
+          setUserInfo(info);
+        }
+      })
+      .catch(err => {
+        isSubscribed = false;
+        return setUserInfo(null);
+      });
+    return () => (isSubscribed = false);
+  }, [memoAuthService]);
+
+  return <RenderCustPro userInfo={userInfo} />;
 };
 
 export default CustProContainer;
