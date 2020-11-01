71c0ebe9352ccdde0c135e78c5be548a65d2bf9b
rdowd <rjarrettdowd@yahoo.com>
2020-10-31 22:57:27 -0400

    Create cutomer Profile Components.

---
+++ src/components/pages/CustomerProfile/RenderCustPro.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RenderCustPro = () => {
+  return <div>test</div>;
+};
+
+export default RenderCustPro;
---
+++ src/components/pages/CustomerProfile/index.js
@@ -0,0 +1,1 @@
+export { default as CustomerProfilePage } from './CustProContainer';
