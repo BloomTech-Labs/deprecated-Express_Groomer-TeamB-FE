3f4ce13a8cffc5bf665e7e8f9646c772baba5e71
rdowd <rjarrettdowd@yahoo.com>
2020-11-02 21:11:50 -0500

    Added customer profile form to profile page. Set up placeholders for profile info. Will render info from okta if no profile info in db.

--- src/components/pages/CustomerProfile/RenderCustPro.js
+++ src/components/pages/CustomerProfile/RenderCustPro.js
@@ -1,7 +1,76 @@
 import React from 'react';
+import { ProfileFormPO } from '../../ProfileFormPO';
+import { Button, Layout } from 'antd';
+import 'antd/dist/antd.css';
 
-const RenderCustPro = () => {
-  return <div>test</div>;
+const RenderCustPro = props => {
+  const {
+    userInfo,
+    isRegistered,
+    custInfo,
+    showForm,
+    toggleForm,
+    updated,
+    setUpdated,
+  } = props;
+  console.log(custInfo, userInfo);
+  return (
+    <div>
+      {showForm ? (
+        <ProfileFormPO
+          info={custInfo}
+          isRegistered={isRegistered}
+          userInfo={userInfo}
+          updated={updated}
+          setUpdated={setUpdated}
+        />
+      ) : null}
+      <Layout.Content
+        style={{
+          background: 'white',
+          width: '75%',
+          margin: '20px auto',
+          padding: '10px',
+        }}
+      >
+        <p>
+          Hello{' '}
+          {custInfo.given_name ? custInfo.given_name : userInfo.given_name}{' '}
+          Welcome to Express Groomer
+        </p>
+        <p>Email: {userInfo.email}</p>
+        <p>
+          First Name:{' '}
+          {custInfo.given_name ? custInfo.given_name : userInfo.given_name}
+        </p>
+        <p>
+          Last Name:{' '}
+          {custInfo.family_name ? custInfo.family_name : userInfo.family_name}
+        </p>
+        <p>
+          Phone Number:{' '}
+          {custInfo.phone_number
+            ? custInfo.phone_number
+            : 'Update your profile'}
+        </p>
+        <p>
+          Address: {custInfo.address ? custInfo.address : 'Update your profile'}
+        </p>
+        <p>City: {custInfo.city ? custInfo.city : 'Update your profile'}</p>
+        <p>State: {custInfo.state ? custInfo.state : 'Update your profile'}</p>
+        <p>
+          Zip Code:{' '}
+          {custInfo.zip_code ? custInfo.zip_code : 'Update your profile'}
+        </p>
+        <p>
+          Country: {custInfo.country ? custInfo.country : 'Update your profile'}
+        </p>
+        <Button type="primary" onClick={() => toggleForm()}>
+          {showForm ? 'Close Form' : 'Update Profile'}
+        </Button>
+      </Layout.Content>
+    </div>
+  );
 };
 
 export default RenderCustPro;
