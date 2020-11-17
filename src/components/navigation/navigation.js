23cad4fc58b1ec2d5f5cdd27002cc2161b60c59b
rdowd <rjarrettdowd@yahoo.com>
2020-11-16 19:43:00 -0500

    Fixed bug

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -24,7 +24,7 @@ function NavBar() {
           console.log('Thi is in navBar useEffect for info.role', info.sub);
         }
         return getUserID(
-          `https:/labspt12-express-groomer-b-api.herokuapp.com/profiles/${info.sub}`,
+          `${process.env.REACT_APP_API_URI}/profiles/${info.sub}`,
           authState
         );
       })
