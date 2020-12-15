3cceb90ab62cd13fbe3ba2c406603b5232b5ce9d
Jess <jessbonanno@gmail.com>
2020-12-14 20:20:17 -0500

    refactor(api index.js): removed unused vars from file

--- src/api/index.js
+++ src/api/index.js
@@ -1,18 +1,10 @@
 import axios from 'axios';
 
 // we will define a bunch of API calls here.
-const apiUrl = `${process.env.REACT_APP_API_URI}`;
 
 const sleep = time =>
   new Promise(resolve => {
     setTimeout(resolve, time);
   });
 
-//example of non-auth API call function
-const getExampleData = () => {
-  return axios
-    .get(`https://jsonplaceholder.typicode.com/photos?albumId=1`)
-    .then(response => response.data);
-};
-
-export {};
+export { sleep };
