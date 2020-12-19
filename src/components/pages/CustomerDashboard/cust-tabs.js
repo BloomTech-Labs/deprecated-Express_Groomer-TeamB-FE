7d32728dd8ae5c567eda8395209e17f6f9de6191
Andre <asf@mail.usf.edu>
2020-12-18 21:35:18 -0500

    Deleted Radio Variable and fix Class to className

--- src/components/pages/CustomerDashboard/cust-tabs.js
+++ src/components/pages/CustomerDashboard/cust-tabs.js
@@ -1,4 +1,4 @@
-import { Tabs, Radio } from 'antd';
+import { Tabs } from 'antd';
 import React from 'react';
 import Overview from "./overview"
 import CustomerProfilePage from "../CustomerProfile/CustProContainer"
@@ -23,18 +23,18 @@ class CustTab extends React.Component {
     return (
       <div>
         <Tabs defaultActiveKey="0" tabPosition={mode} style={{ height: "100%", marginLeft: "5%"}}>
-            <TabPane style={{ fontSize: "16px"}} tab={<span><i class="fas fa-paw"></i> Overview</span>} key="0">
+            <TabPane style={{ fontSize: "16px"}} tab={<span><i className="fas fa-paw"></i> Overview</span>} key="0">
               <Overview/>
             </TabPane>
-            <TabPane tab={<span><i class="fas fa-paw"></i> My Info</span>} key="1">
+            <TabPane tab={<span><i className="fas fa-paw"></i> My Info</span>} key="1">
               <CustomerProfilePage/> 
-            </TabPane><TabPane tab={<span><i class="fas fa-paw"></i> My Pets</span>} key="2">
+            </TabPane><TabPane tab={<span><i className="fas fa-paw"></i> My Pets</span>} key="2">
               My Pets
             </TabPane>
-            <TabPane tab={<span><i class="fas fa-paw"></i> Appointments</span>} key="3">
+            <TabPane tab={<span><i className="fas fa-paw"></i> Appointments</span>} key="3">
               Appointments
             </TabPane>
-            <TabPane tab={<span><i class="fas fa-paw"></i> Search Groomers</span>} key="4">
+            <TabPane tab={<span><i className="fas fa-paw"></i> Search Groomers</span>} key="4">
               Search Groomers
             </TabPane>
         </Tabs>
