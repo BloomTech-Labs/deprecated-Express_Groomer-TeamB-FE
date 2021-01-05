48b8f86cb7e6235493b266b0e0ea4e22bb14db6e
Jess Bonanno <58790413+JessBonanno@users.noreply.github.com>
2021-01-04 22:15:05 -0500

    Create pet form (#34)
    * feat(petform): created folder and basic files for petform
    
    getting scaffolding ready for the pet form component
    
    * feat(petform): added basic model
    
    * feat(petform): added form to modal
    
    * feat(petform): for properly recieving and saving values
    
    * refactor(petform): refactored most state and functions into form context
    
    * refactor(petform): changed button text to be more descriptive
    
    * feat(petform): pet form now posting to API
    
    * refactor(petform): changed order of input fields
    
    * feat(cust-tabs): add pet added to pet tab in dash
    
    * chore(petform): removed unused vars
    
    * refactor(project wide): removed storybook files

---
+++ src/components/pages/PetForm/PetFormContainer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import RenderPetForm from './RenderPetForm';
+
+const PetFormContainer = () => {
+  return <RenderPetForm />;
+};
+
+export default PetFormContainer;
---
+++ src/components/pages/PetForm/RenderPetForm.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import PetFormModal from './PetFormModal';
+
+const RenderPetForm = () => {
+  return <PetFormModal />;
+};
+
+export default RenderPetForm;
