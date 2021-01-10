3545451d8fdf954c4bc382236cf199b084eb57d2
Jess <jessbonanno@gmail.com>
2021-01-09 20:10:49 -0500

    feat(groomer map): added heading to map page

--- src/components/pages/GroomerMap/GroomerMapContainer.js
+++ src/components/pages/GroomerMap/GroomerMapContainer.js
@@ -2,8 +2,9 @@ import React, { useContext, useEffect, useState } from 'react';
 import ReactMapGL, { GeolocateControl, Marker, Popup } from 'react-map-gl';
 import { GroomersContext } from '../../../state/contexts/GroomersContext';
 import { APIContext } from '../../../state/contexts/APIContext';
-import { Button } from 'antd';
+import { Button, Col, Row } from 'antd';
 import { ScissorOutlined } from '@ant-design/icons';
+import './GroomerMap.css';
 
 const GroomerMap = () => {
   const { allGroomers } = useContext(GroomersContext);
@@ -13,8 +14,8 @@ const GroomerMap = () => {
   const [viewport, setViewport] = useState({
     latitude: 40.7,
     longitude: -74.0,
-    width: '100vw',
-    height: '100vh',
+    width: '800px',
+    height: '500px',
     zoom: 3,
   });
 
@@ -37,64 +38,71 @@ const GroomerMap = () => {
   }, []);
 
   return (
-    <div>
-      <ReactMapGL
-        {...viewport}
-        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-        mapStyle={process.env.REACT_APP_MAPBOX_STYLE}
-        onViewportChange={viewport => {
-          setViewport(viewport);
-        }}
-      >
-        <GeolocateControl
-          positionOptions={{ enableHighAccuracy: true }}
-          trackUserLocation={true}
-        />
-        {allGroomers &&
-          allGroomers.map(groomer => {
-            return (
-              <Marker
-                key={groomer.id}
-                latitude={groomer.lat}
-                longitude={groomer.lng}
-              >
-                <Button
-                  icon={<ScissorOutlined />}
-                  type={'primary'}
-                  className={'marker-btn'}
-                  onClick={e => {
-                    e.preventDefault();
-                    setSelectedGroomer(groomer);
-                  }}
-                >
-                  {groomer.business_name}
-                </Button>
-              </Marker>
-            );
-          })}
-        {selectedGroomer ? (
-          <Popup
-            latitude={selectedGroomer.lat}
-            longitude={selectedGroomer.lng}
-            onClose={() => {
-              setSelectedGroomer(null);
+    <>
+      <h2 className={'map-heading'}>
+        Use the locator button to zoom to your location
+      </h2>
+      <Row justify={'center'} className={'map-container'}>
+        <Col>
+          <ReactMapGL
+            {...viewport}
+            mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+            mapStyle={process.env.REACT_APP_MAPBOX_STYLE}
+            onViewportChange={viewport => {
+              setViewport(viewport);
             }}
           >
-            <div>
-              <h2>{selectedGroomer.business_name}</h2>
-              <h3>
-                {selectedGroomer.address}
-                <br /> {selectedGroomer.city}, {selectedGroomer.state}{' '}
-                {selectedGroomer.zip_code}
-              </h3>
-              <h3>{selectedGroomer.email}</h3>
-              <h3>{selectedGroomer.phone_number}</h3>
-              <p style={{ maxWidth: '350px' }}>{selectedGroomer.about}</p>
-            </div>
-          </Popup>
-        ) : null}
-      </ReactMapGL>
-    </div>
+            <GeolocateControl
+              positionOptions={{ enableHighAccuracy: true }}
+              trackUserLocation={true}
+            />
+            {allGroomers &&
+              allGroomers.map(groomer => {
+                return (
+                  <Marker
+                    key={groomer.id}
+                    latitude={groomer.lat}
+                    longitude={groomer.lng}
+                  >
+                    <Button
+                      icon={<ScissorOutlined />}
+                      type={'primary'}
+                      className={'marker-btn'}
+                      onClick={e => {
+                        e.preventDefault();
+                        setSelectedGroomer(groomer);
+                      }}
+                    >
+                      {groomer.business_name}
+                    </Button>
+                  </Marker>
+                );
+              })}
+            {selectedGroomer ? (
+              <Popup
+                latitude={selectedGroomer.lat}
+                longitude={selectedGroomer.lng}
+                onClose={() => {
+                  setSelectedGroomer(null);
+                }}
+              >
+                <div>
+                  <h2>{selectedGroomer.business_name}</h2>
+                  <h3>
+                    {selectedGroomer.address}
+                    <br /> {selectedGroomer.city}, {selectedGroomer.state}{' '}
+                    {selectedGroomer.zip_code}
+                  </h3>
+                  <h3>{selectedGroomer.email}</h3>
+                  <h3>{selectedGroomer.phone_number}</h3>
+                  <p style={{ maxWidth: '350px' }}>{selectedGroomer.about}</p>
+                </div>
+              </Popup>
+            ) : null}
+          </ReactMapGL>
+        </Col>
+      </Row>
+    </>
   );
 };
 
