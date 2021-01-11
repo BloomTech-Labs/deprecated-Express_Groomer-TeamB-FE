e574b150b5325dce00c91bbf5992cc6c35d740e5
Nick Verdin <nv1028@aggienetwork.com>
2021-01-11 15:50:07 -0600

    initial set up of groomer dashboard

---
+++ src/components/pages/GroomerDashboard/overview.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './overview.scss';
+
+export default function Overview() {
+  return (
+    <div>
+      <div className="Upcoming-Appointments">
+        <h1>Upcoming Appointments</h1>
+      </div>
+      <div className="Current-Balance">
+        <h1>Current Balance</h1>
+      </div>
+    </div>
+  );
+}
