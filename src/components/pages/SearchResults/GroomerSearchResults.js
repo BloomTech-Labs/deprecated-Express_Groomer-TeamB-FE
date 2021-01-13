d2d1b32e8a44d984095bb83f5db56fe0a8d8f3c1
Andre <asf@mail.usf.edu>
2021-01-13 17:12:32 -0500

    Added Styling to GroomerSearchResults

--- src/components/pages/SearchResults/GroomerSearchResults.js
+++ src/components/pages/SearchResults/GroomerSearchResults.js
@@ -4,6 +4,7 @@ import { UserOutlined } from '@ant-design/icons';
 import 'antd/dist/antd.css';
 import '../GroomerProfile/groomer.css';
 import Services from '../GroomerProfile/ServicesArea';
+import './GroomerPublicProfile.scss'
 // context imports
 import { GroomersContext } from '../../../state/contexts/GroomersContext';
 import { APIContext } from '../../../state/contexts/APIContext';
@@ -21,58 +22,66 @@ const GroomerSearchResults = props => {
 
   if (groomer) {
     return (
-      <div>
+      <div className="groomer-public-box">
         <Layout.Content
           style={{
             background: 'white',
             width: '75%',
             margin: '20px auto',
-            padding: '10px',
+            padding: '2%',
           }}
         >
 
           <div className="customer-header">
             <p className="heading"> {groomer.given_name} </p>
             <div className="rating">
-              <h1>THIS WILL BE RATING</h1>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
             </div>
             <div className="avatar">
             <Avatar size={74} icon={<UserOutlined />} />
           </div>
           </div>
           <div className="customer-info-box">
-            <div></div>
-            <div className="panel">
-              <Divider style={{ borderColor: 'lightblue' }}>
-              About
-              </Divider>
-              <div className="panel-info">
-                <p>This will be about me section</p>
+            <div className="top-info-box">
+              <div className="panel">
+                <Divider style={{ borderColor: 'lightblue' }}>
+                About
+                </Divider>
+                <div className="panel-info">
+                  <p>This will be about me section</p>
+                </div>
+              </div>
+              <div className="panel">
+                <Divider style={{ borderColor: 'lightblue' }}>Services</Divider>
+                <div className="panel-info">
+                  <Services />
+                </div>
               </div>
             </div>
-            <div className="panel">
-              <Divider style={{ borderColor: 'lightblue' }}>Services</Divider>
-              <div className="panel-info">
-                <Services />
+            <div className="bottom-info-box">
+              <div className="panel">
+                <Divider style={{ borderColor: 'lightblue' }}>Location</Divider>
+                <div className="panel-info">
+                  <p>Address: {groomer.address}</p>
+                  <p>City: {groomer.city}</p>
+                  <p>State: {groomer.state}</p>
+                  <p>Zip Code: {groomer.zip_code}</p>
+                  <p>Country: {groomer.country}</p>
+                </div>
               </div>
-            </div>
-            <div className="panel">
-              <Divider style={{ borderColor: 'lightblue' }}>Location</Divider>
-              <div className="panel-info">
-                <p>Address: {groomer.address}</p>
-                <p>City: {groomer.city}</p>
-                <p>State: {groomer.state}</p>
-                <p>Zip Code: {groomer.zip_code}</p>
-                <p>Country: {groomer.country}</p>
+              <div className="panel">
+                <Divider style={{ borderColor: 'lightblue' }}>
+                Hours
+                </Divider>
+                <div className="panel-info">
+                  <p>Hours will go here</p>
+                </div>
               </div>
-            </div>
-            <div className="panel">
-              <Divider style={{ borderColor: 'lightblue' }}>
-              Hours
-              </Divider>
-              <div className="panel-info">
-                <p>Hours will go here</p>
-              </div>
+              <button className="appt-button">Schedule Appointment</button>
             </div>
           </div>
         </Layout.Content>
