d73f97fc769cc9b7d39806702db669233af386fb
Jess <jessbonanno@gmail.com>
2021-01-17 14:41:45 -0500

    feat(calendlypopupwidget): created calendly widget for scheduling appointments

---
+++ src/components/common/CalendlyPopupWidget.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { PopupWidget } from 'react-calendly';
+
+// this component will render the calendly scheduler
+// it needs to be passed a schedule link as props
+// this comes from the groomer information as:
+// groomerInfo.personal_calendly_link
+const CalendlyPopupWidget = ({ scheduleLink }) => {
+  return (
+    <div>
+      <PopupWidget url={scheduleLink} text={'Schedule Appointment'} />
+    </div>
+  );
+};
+
+export default CalendlyPopupWidget;
