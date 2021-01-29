a2a5611b870352345ce44ea8d70d9a348d24e44f
timotheemcdonald <timotheemcdonald@gmail.com>
2021-01-28 20:44:48 -0500

    setting up PetName display indepedent of card

---
+++ src/components/pages/PetForm/PetNameLink.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './petcard.css';
+
+const PetNameLink = () => {
+  return (
+    <div className="petLink">
+      <h2>Pet Name {}</h2>
+    </div>
+  );
+};
+
+export default PetNameLink;
